cef938848d9496dff8c516c8a1ddd2573efbb8a8
Arun1998k <arun1998k@gmail.com>
2022-04-30 01:17:29 +0530

    Employee status

---
+++ UC1EmpStatus/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+
+        int empCheck = random.Next(2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is presant");
+
+        }
+        else
+        {
+            Console.WriteLine("Employee is absand");
+
+        }
+    }
+}
