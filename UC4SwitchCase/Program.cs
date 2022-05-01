732b9480bcfb35bcc4f404862ed064dbe8322494
Arun1998k <arun1998k@gmail.com>
2022-05-01 15:59:22 +0530

    UC4 Switch Case

---
+++ UC4SwitchCase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace EmpSwitchCase {
+    public class Program {
+        public const int IS_PATR_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
+        static void Main(string[] args) {
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_PATR_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
+
+
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+
+         }
+    }
+}
