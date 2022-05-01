fc049b76ad82cb5680bf7c01da3227a5b4186a4c
Arun1998k <arun1998k@gmail.com>
2022-05-01 15:18:14 +0530

    Uc2

---
+++ UC2EmpWage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace UCEmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+
+            }
+            empWage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp wage : " + empWage);
+        }
+    }
+}
